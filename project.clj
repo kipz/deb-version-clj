b9c4bbddccdea7c4c47f408a160eb71e8d73d833
James Carnegie <james.carnegie@docker.com>
2023-05-04 14:53:32 +0100

    Version 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.kipz/deb-version-clj "0.0.4-SNAPSHOT"
+(defproject org.kipz/deb-version-clj "0.0.4"
   :description "Parse and compare debian package versions in clojure"
   :url "https://github.com/kipz/deb-version-clj"
   :license {:name "Eclipse Public License"
